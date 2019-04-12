01aaeae39f06f90f8a1f5ffd5363f0ebd5c75cd6
Tien Tran <tientran2407.vn@gmail.com>
2019-04-12 22:25:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+class BroadMember {
+  constructor (name, homeState, training){
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training
+  }
+  
+  veto = (){
+    
+  }
+}
